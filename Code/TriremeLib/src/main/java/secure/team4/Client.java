32e944d9a1b9a672c66d311db67632725347de55
Justin Petry <justincpetry@gmail.com>
2024-10-10 18:34:25 -0500

    even better EOF handling (uses handshake)

--- Code/TriremeLib/src/main/java/secure/team4/Client.java
+++ Code/TriremeLib/src/main/java/secure/team4/Client.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Client {
-    private final int PACKET_SIZE = 8;
+    private final int PACKET_SIZE = 1000;
     public Client(String hostName, int port, String file) throws IOException {
         int fileIndex = 0;
         // TODO files are limited to 2 GiB
@@ -18,25 +18,12 @@ public class Client {
                 DataInputStream in = new DataInputStream(socket.getInputStream());
                 DataOutputStream out = new DataOutputStream(
                         socket.getOutputStream())) {
+            out.writeInt(fileContent.length);
             while(fileContent.length > fileIndex) {
-                if (fileContent.length > fileIndex + PACKET_SIZE) {
-                    out.write(fileContent, fileIndex, PACKET_SIZE); // write full-size packet
-                }
-                else {
-                    byte[] finalContent = new byte[PACKET_SIZE];
-                    Arrays.fill(finalContent, (byte) 0x00);
-                    // if we reach the end of file, create a full-size packet
-                    // with EOF char at the end
-                    // (there may be a better way to do this)
-                    for (int i = 0; i < PACKET_SIZE-1; i++) {
-                        finalContent[i] = (fileIndex + i < fileContent.length) ? fileContent[fileIndex+i] : 0x06;
-                    }
-                    out.write(finalContent, 0, PACKET_SIZE);
-                    socket.close();
-                }
+                out.write(fileContent, fileIndex, Math.min(PACKET_SIZE, fileContent.length - fileIndex)); // write full-size packet
                 // acknowledge OK signal
                 in.readNBytes(2);
-                fileIndex = fileIndex + 8;
+                fileIndex = fileIndex + PACKET_SIZE;
             }
         } catch (SocketException e) {
             // When the server closes the socket, exit
--- Code/TriremeLib/src/main/java/secure/team4/Server.java
+++ Code/TriremeLib/src/main/java/secure/team4/Server.java
@@ -1,9 +1,6 @@
 package secure.team4;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
@@ -11,25 +8,33 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Server {
-    private final int PACKET_SIZE = 8;
+    private final int PACKET_SIZE = 1000;
 
     public Server(int port) throws IOException {
+        int fileSize = 0;
+        int index = 0;
         try (ServerSocket server = new ServerSocket(port);
              Socket clientSocket = server.accept(); // open socket
              DataInputStream in = new DataInputStream(
-                     new BufferedInputStream(clientSocket.getInputStream()))) {
-            DataOutputStream out = new DataOutputStream(
-                    clientSocket.getOutputStream());
+                     new BufferedInputStream(clientSocket.getInputStream()));
+             DataOutputStream out = new DataOutputStream(
+                     clientSocket.getOutputStream());
+             FileOutputStream fileOut = new FileOutputStream("output");
+        )
+        {
             {
                 byte[] packet = new byte[PACKET_SIZE];
                 Arrays.fill(packet, (byte) 0x00);
+                fileSize = in.readInt();
                 while (true) {
-                    packet = in.readNBytes(PACKET_SIZE); // read in a packet
+                    packet = in.readNBytes(Math.min(fileSize - index, PACKET_SIZE)); // read in a packet
                     out.writeUTF("OK"); // packet received successfully
-                    System.out.print(new String(packet, StandardCharsets.US_ASCII));
-                    for (byte b : packet) {
-                        if (b == 0x06) clientSocket.close();
+                    fileOut.write(packet);
+                    if (fileSize - index < PACKET_SIZE) {
+                        clientSocket.close();
+                        break;
                     }
+                    index += PACKET_SIZE;
                 }
             }
         } catch (SocketException e) {
