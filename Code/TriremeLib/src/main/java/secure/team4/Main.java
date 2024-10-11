c7d03793aa95a3277226489b9445c7fecb66857f
Justin Petry <justincpetry@gmail.com>
2024-10-10 22:55:02 -0500

    add args and document them

--- Code/TriremeLib/src/main/java/secure/team4/Main.java
+++ Code/TriremeLib/src/main/java/secure/team4/Main.java
@@ -1,19 +1,18 @@
 package secure.team4;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.security.NoSuchAlgorithmException;
-import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         if (args[0].equals("send")) {
-            new Client("127.0.0.1", 15420, args[1]);
-        } else {
-            new Server(15420);
+            // send [hostname] [port] [filepath]
+            new Client(args[1], Integer.parseInt(args[2]), args[3]);
+        } else if (args[0].equals("receive")) {
+            // receive [port]
+            new Server(Integer.parseInt(args[1]));
         }
     }
 }
