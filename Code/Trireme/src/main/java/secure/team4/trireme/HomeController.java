4dede6a0c2ed784ec59934686de524c04974f440
lexi <kindaconfusion@gmail.com>
2024-10-14 17:35:04 -0500

    renaming gui app

--- Code/Trireme/src/main/java/secure/team4/trireme/HomeController.java
+++ Code/Trireme/src/main/java/secure/team4/trireme/HomeController.java
@@ -3,7 +3,7 @@ package secure.team4.trireme;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HomeController {
     @FXML
     private Label welcomeText;
 
--- Code/Trireme/src/main/java/secure/team4/trireme/TriremeApplication.java
+++ Code/Trireme/src/main/java/secure/team4/trireme/TriremeApplication.java
@@ -8,14 +8,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TriremeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TriremeApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Trireme");
         stage.setScene(scene);
         stage.show();
     }
