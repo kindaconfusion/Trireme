4a4e81031f352cf950c76fac9e44e1315a50e61b
lexi <kindaconfusion@gmail.com>
2024-10-16 17:09:53 -0500

    simple UI (nonfunctional)

--- Code/Trireme/src/main/java/secure/team4/trireme/HomeController.java
+++ Code/Trireme/src/main/java/secure/team4/trireme/HomeController.java
@@ -1,14 +1,35 @@
 package secure.team4.trireme;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
 
 public class HomeController {
+    public TextField sendPort;
+    public TextField recvPort;
+    public Button selectFileBtn;
+    public TextField sendHost;
+    public Button sendFileBtn;
+    public TextField fileField;
     @FXML
     private Label welcomeText;
+    final FileChooser fileChooser = new FileChooser();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSelectButtonClick(ActionEvent ae) {
+        Node source = (Node) ae.getSource();
+        Window stage = source.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        fileField.setText(file.getAbsolutePath());
+    }
+
+    public void onSendButtonClick(ActionEvent actionEvent) {
     }
 }
